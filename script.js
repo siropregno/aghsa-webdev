00dd2b1ec431c496937e3dd28a974392a9da8f87
Siro Pregno <siropregno14@gmail.com>
2024-08-23 16:16:04 -0300

    estructura mobile

---
+++ script.js
@@ -0,0 +1,11 @@
+//DROPDOWN HOME
+
+function toggleDropdown() {
+    const dropdownContent = document.getElementById("myDropdown");
+    dropdownContent.classList.toggle("show");
+}
+
+function toggleDropdownB() {
+    const dropdownContent = document.getElementById("myDropdownB");
+    dropdownContent.classList.toggle("showB");
+}
