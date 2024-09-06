aacd35af8cb628417cffc8c1a6feeedf60113822
Agustin Pagliuca <agustinpagliuca1@gmail.com>
2024-09-06 16:22:51 -0300

    bootstrap

--- script.js
+++ script.js
@@ -25,8 +25,3 @@ function toggleDropdown() {
     const dropdownContent = document.getElementById("myDropdown");
     dropdownContent.classList.toggle("show");
 }
-
-function toggleDropdownB() {
-    const dropdownContent = document.getElementById("myDropdownB");
-    dropdownContent.classList.toggle("showB");
-}
